dbe76e3c23f3af43cc9b70aaf8a4db6468cebd97
Darianna Cruz De La Rosa <darianna_cruz@ucne.edu.do>
2021-10-31 11:10:26 -0400

    Se completo la Entidad Aportes

--- Entidades/Aportes.cs
+++ Entidades/Aportes.cs
@@ -3,11 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GestionPersonas.Entidades
 {
     public class Aportes
     {
-        //AporteId,Fecha,PersonaId,Concepto, Monto
+        [Key]
+        public int AporteId { get; set; }
+        public DateTime Fecha { get; set; }
+        public int PersonaId { get; set; }
+        public string Concepto { get; set; }
+        public double Monto { get; set; }
+
+        [ForeignKey("TipoAporteId")]
+        public List<AportesDetalle> Detalle { get; set; } = new List<AportesDetalle>();
+
+        public Aportes()
+        {
+            AporteId = 0;
+            Fecha = DateTime.Now;
+            PersonaId = 0;
+            Concepto = string.Empty;
+            Monto = 0;
+        }
+
+        public Aportes(int aporteId, DateTime fecha, int personaId, string concepto, double monto)
+        {
+            AporteId = aporteId;
+            Fecha = fecha;
+            PersonaId = personaId;
+            Concepto = concepto;
+            Monto = monto;
+        }
+
     }
 }
