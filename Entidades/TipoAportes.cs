219148b56fdd144f6f85275cd990d485c17884fb
Darianna Cruz De La Rosa <darianna_cruz@ucne.edu.do>
2021-10-31 10:51:02 -0400

    Se creo la entidad Tipo Aportes

---
+++ Entidades/TipoAportes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace GestionPersonas.Entidades
+{
+    public class TipoAportes
+    {
+        [Key]
+        public int TipoAporteId { get; set; }
+        public string Descripcion { get; set; }
+
+
+        public TipoAportes()
+        {
+            TipoAporteId = 0;
+            Descripcion = string.Empty;
+
+
+        }
+        public TipoAportes(int tipoAporteId, string descripcion)
+        {
+            TipoAporteId = tipoAporteId;
+            Descripcion = descripcion;
+
+        }
+    }
+}
